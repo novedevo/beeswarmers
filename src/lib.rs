f1c93f2569f3693c1cb9e972f1ff10b9b544be9e
Devon Sawatsky <novedevo@gmail.com>
2024-06-18 15:11:08 -0700

    https://i.imgur.com/0RRdP.gif

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use std::ops::Range;
 
 /// input slice of points must be sorted, radius must not be zero
 /// if either of these preconditions is invalidated, this function may panic or return nonsensical results.
+/// returns a list of bees (multidimensional points plottable on your graph)
 pub fn beeswarm<const DIM: usize>(sorted_points: &[f64], radius: f64) -> Vec<[f64; DIM]> {
     let ranges = sorted_points
         .iter()
@@ -10,36 +11,36 @@ pub fn beeswarm<const DIM: usize>(sorted_points: &[f64], radius: f64) -> Vec<[f6
                 .expect("there should be at least one point within its radius-- itself")
         })
         .collect::<Vec<_>>();
-    let mut retval = vec![[0.0; DIM]; sorted_points.len()];
+    let mut swarm = vec![[0.0; DIM]; sorted_points.len()];
     for (i, &point) in sorted_points.iter().enumerate() {
-        let mut next_point = [0.0; DIM];
-        next_point[0] = point;
-        let intersecting_points =
-            intersections(&retval, radius, (i, next_point), ranges[i].clone());
+        let mut next_bee = [0.0; DIM];
+        next_bee[0] = point;
+        let intersecting_bees =
+            intersections(&swarm, radius, (i, next_bee), ranges[i].clone());
 
-        retval[i] = if intersecting_points.is_empty() {
-            next_point
+        swarm[i] = if intersecting_bees.is_empty() {
+            next_bee
         } else {
             jiggle(
-                &retval,
-                intersecting_points,
+                &swarm,
+                intersecting_bees,
                 radius,
                 ranges[i].clone(),
-                next_point,
+                next_bee,
             )
         }
     }
-    retval
+    swarm
 }
 
 fn jiggle<const DIM: usize>(
-    placed_points: &[[f64; DIM]],
-    mut intersecting_points: Vec<[f64; DIM]>,
+    placed_bees: &[[f64; DIM]],
+    mut intersecting_bees: Vec<[f64; DIM]>,
     radius: f64,
     range: Range<usize>,
-    new_point: [f64; DIM],
+    new_bee: [f64; DIM],
 ) -> [f64; DIM] {
-    let index = placed_points.len();
+    let index = placed_bees.len();
     let mut candidate = None;
     let mut iteration = 0.0;
     let jigglestep = radius / 2.0;
@@ -47,14 +48,14 @@ fn jiggle<const DIM: usize>(
         iteration += 1.0;
         candidate = (1..DIM)
             .flat_map(|i| {
-                let mut candidate1 = new_point;
-                let mut candidate2 = new_point;
+                let mut candidate1 = new_bee;
+                let mut candidate2 = new_bee;
                 candidate1[i] += iteration * jigglestep;
                 candidate2[i] -= iteration * jigglestep;
                 [candidate1, candidate2]
             })
             .find(|candidate| {
-                intersections(placed_points, radius, (index, *candidate), range.clone()).is_empty()
+                intersections(placed_bees, radius, (index, *candidate), range.clone()).is_empty()
             });
     }
     candidate.unwrap()
@@ -63,26 +64,26 @@ fn jiggle<const DIM: usize>(
 //fn get_positions_along_radius_higher_dimensional
 
 fn intersections<const DIM: usize>(
-    placed_points: &[[f64; DIM]],
+    placed_bees: &[[f64; DIM]],
     radius: f64,
-    new_point: (usize, [f64; DIM]),
+    new_bee: (usize, [f64; DIM]),
     range: Range<usize>,
 ) -> Vec<[f64; DIM]> {
-    placed_points
+    placed_bees
         .into_iter()
         .enumerate()
-        .filter(|(i, placed_point)| {
+        .filter(|(i, placed_bee)| {
             !(!range.contains(i)
-                || *i == new_point.0
-                || euclidean_distance(**placed_point, new_point.1) > radius)
+                || *i == new_bee.0
+                || euclidean_distance(**placed_bee, new_bee.1) > radius)
         })
-        .map(|(_, point)| *point)
+        .map(|(_, bee)| *bee)
         .collect()
 }
 
-fn euclidean_distance<const DIM: usize>(point1: [f64; DIM], point2: [f64; DIM]) -> f64 {
+fn euclidean_distance<const DIM: usize>(bee1: [f64; DIM], bee2: [f64; DIM]) -> f64 {
     (0..DIM)
-        .map(|i| (point1[i] - point2[i]).powi(2))
+        .map(|i| (bee1[i] - bee2[i]).powi(2))
         .sum::<f64>()
         .sqrt()
 }
