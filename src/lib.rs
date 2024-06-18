5d71ac514a82647324381eb5e89443d733f89181
Devon Sawatsky <novedevo@gmail.com>
2024-06-18 15:04:53 -0700

    simple jiggle only on cardinal directions with set jigglestep

--- src/lib.rs
+++ src/lib.rs
@@ -10,27 +10,74 @@ pub fn beeswarm<const DIM: usize>(sorted_points: &[f64], radius: f64) -> Vec<[f6
                 .expect("there should be at least one point within its radius-- itself")
         })
         .collect::<Vec<_>>();
-    let mut retval = vec![];
+    let mut retval = vec![[0.0; DIM]; sorted_points.len()];
     for (i, &point) in sorted_points.iter().enumerate() {
         let mut next_point = [0.0; DIM];
         next_point[0] = point;
-        if !intersects(&retval, radius, (i, next_point), ranges[i].clone()) {
-            retval.push(next_point)
+        let intersecting_points =
+            intersections(&retval, radius, (i, next_point), ranges[i].clone());
+
+        retval[i] = if intersecting_points.is_empty() {
+            next_point
+        } else {
+            jiggle(
+                &retval,
+                intersecting_points,
+                radius,
+                ranges[i].clone(),
+                next_point,
+            )
         }
     }
     retval
 }
 
-fn intersects<const DIM: usize>(
+fn jiggle<const DIM: usize>(
+    placed_points: &[[f64; DIM]],
+    mut intersecting_points: Vec<[f64; DIM]>,
+    radius: f64,
+    range: Range<usize>,
+    new_point: [f64; DIM],
+) -> [f64; DIM] {
+    let index = placed_points.len();
+    let mut candidate = None;
+    let mut iteration = 0.0;
+    let jigglestep = radius / 2.0;
+    while candidate.is_none() {
+        iteration += 1.0;
+        candidate = (1..DIM)
+            .flat_map(|i| {
+                let mut candidate1 = new_point;
+                let mut candidate2 = new_point;
+                candidate1[i] += iteration * jigglestep;
+                candidate2[i] -= iteration * jigglestep;
+                [candidate1, candidate2]
+            })
+            .find(|candidate| {
+                intersections(placed_points, radius, (index, *candidate), range.clone()).is_empty()
+            });
+    }
+    candidate.unwrap()
+}
+
+//fn get_positions_along_radius_higher_dimensional
+
+fn intersections<const DIM: usize>(
     placed_points: &[[f64; DIM]],
     radius: f64,
     new_point: (usize, [f64; DIM]),
     range: Range<usize>,
-) -> bool {
-    placed_points.iter().enumerate().all(|(i, placed_point)| {
-        range.contains(&i)
-            && (i == new_point.0 || euclidean_distance(*placed_point, new_point.1) > radius)
-    })
+) -> Vec<[f64; DIM]> {
+    placed_points
+        .into_iter()
+        .enumerate()
+        .filter(|(i, placed_point)| {
+            !(!range.contains(i)
+                || *i == new_point.0
+                || euclidean_distance(**placed_point, new_point.1) > radius)
+        })
+        .map(|(_, point)| *point)
+        .collect()
 }
 
 fn euclidean_distance<const DIM: usize>(point1: [f64; DIM], point2: [f64; DIM]) -> f64 {
